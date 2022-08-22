da861115b990645e3e02d885ff1bb756aea4b054
Valentin Schoop <todogrubb@web.de>
2022-08-22 14:23:58 +0200

    CNN notebook for slice-pick testing

--- preprocessing/getdata.py
+++ preprocessing/getdata.py
@@ -3,22 +3,37 @@ import pandas as pd
 import nibabel as nib
 import os
 
-def get_csvdata():
+def get_csvdata(drop_young=True, drop_contradictions=True):
+    '''
+    Loads the .csv dataset and returns a preprocessed dataframe.
+        
+        Parametes: drop_young (if true, removes entries with age < 60)
+        
+        Processing steps:
+            NaNs in "CDR" are replaced by 0
+            Remove entries of young patients (Optional)
+            Remove entries where CDR and MMSE results contradict each other
+        
+        Returns: the processed Dataframe
+    '''
     df = pd.read_csv('../data/oasis_cross-sectional.csv')
     df['CDR'].fillna(0, inplace=True)
-    df=df[df['Age']>=60]
-    df = df[((df['CDR']==1.0) & (df['MMSE']<29)) | ((df['CDR']==0.5) & (df['MMSE']<30)) | ((df['CDR']==0.0) & (df['MMSE']>26))]
+    if drop_young:
+        df=df[df['Age']>=60]
+    if drop_contradictions:
+        df = df[((df['CDR']==1.0) & (df['MMSE']<29)) | ((df['CDR']==0.5) & (df['MMSE']<30)) | ((df['CDR']==0.0) & (df['MMSE']>26))]
     df['CDR']=(df['CDR']>0).astype(int)
     return df
 
-def get_slices(IDs, N=0, d=2, dim=0, m=100):
+def get_slices(IDs, N=0, d=1, dim=0, m=95):
     '''
-    Returns slices of 3D-images at given Paths
+    Returns slices of masked 3D-images at given Paths
         Parameters:
                 IDs: list of paths 
                 N: number of steps in each direction
                 d: step size
                 dim: axis along which the image is sliced
+                    0= sagittal, 1= cortical, 2= traverse
                 m: starting slice
 
         Returns: 
